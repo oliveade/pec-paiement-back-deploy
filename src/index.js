73a3cd3d43821f531527b398d71b4874ff088724
oliveade <olive.ade@eemi.com>
2025-07-24 15:16:03 +0200

    admin create

--- src/index.js
+++ src/index.js
@@ -58,5 +58,6 @@ app.listen(PORT, async () => {
   console.log(`Serveur lancé sur http://localhost:${PORT}`);
   await connectPostgres();
   await syncDb();
+  await createDefaultAdmin;
   await connectMongo();
 });
