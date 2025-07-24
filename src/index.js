3d86a660548241a122a617f3716467a2caed5816
oliveade <olive.ade@eemi.com>
2025-07-24 23:28:43 +0200

    syntax

--- src/index.js
+++ src/index.js
@@ -61,6 +61,5 @@ app.listen(PORT, async () => {
   console.log(`Serveur lancé sur http://localhost:${PORT}`);
   await connectPostgres();
   await syncDb();
-  await createDefaultAdmin();
   await connectMongo();
 });
