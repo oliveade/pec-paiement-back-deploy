d7ced40273c5b59d285847b546571224fd18bd4f
oliveade <olive.ade@eemi.com>
2025-07-24 19:16:00 +0200

    admin create role

--- src/index.js
+++ src/index.js
@@ -59,6 +59,5 @@ app.listen(PORT, async () => {
   console.log(`Serveur lancé sur http://localhost:${PORT}`);
   await connectPostgres();
   await syncDb();
-  await createDefaultAdmin();
   await connectMongo();
 });
